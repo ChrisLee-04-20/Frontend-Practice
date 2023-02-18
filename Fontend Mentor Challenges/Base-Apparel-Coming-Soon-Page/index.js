eeada1967461109ef34d8ae72b5990fea422e9f3
ChrisLee <98399268+ChrisLee-04-20@users.noreply.github.com>
2023-02-18 14:48:52 +0800

    Add files via upload

---
+++ Fontend Mentor Challenges/Base-Apparel-Coming-Soon-Page/index.js
@@ -0,0 +1,27 @@
+const email = document.getElementById("email");
+const error_icon = document.getElementById("error-icon");
+const error_message = document.getElementById("error-message");
+
+function isEmailValid() {
+    const emailValue = email.value;
+
+    if (!emailValue.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        displayErrorMessage()
+    } else {
+        noErrorMessage();
+    }
+
+    if(!emailValue){
+        noErrorMessage();
+    }
+}
+
+function noErrorMessage() {
+    error_icon.style.display = "none";
+    error_message.style.visibility = "hidden";
+}
+
+function displayErrorMessage() {
+    error_icon.style.display = "block";
+    error_message.style.visibility = "visible";
+}
