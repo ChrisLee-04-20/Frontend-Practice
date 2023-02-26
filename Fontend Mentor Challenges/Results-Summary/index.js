4652e385d874138b31fd3261ecc9ed578da7b6cc
ChrisLee <98399268+ChrisLee-04-20@users.noreply.github.com>
2023-02-26 18:43:21 +0800

    first commit

---
+++ Fontend Mentor Challenges/Results-Summary/index.js
@@ -0,0 +1,14 @@
+const summaryLabels = document.getElementsByClassName("summary-labels");
+
+fetch ("./data.json")
+    .then( res => res.json())
+    .then(data => {
+        data.forEach(element => {
+            const summarySubtitle = element.category;
+            const summaryPoint = element.score;
+            const summaryIcon = element.icon;  
+
+            const summaryLabel = document.createElement("div");
+
+        });
+    })
