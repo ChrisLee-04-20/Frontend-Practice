4879f4449a1d971b2941ddfe171cd43795cd1ae4
ChrisLee <98399268+ChrisLee-04-20@users.noreply.github.com>
2023-03-18 22:25:03 +0800

    finish the overall website

--- Fontend Mentor Challenges/Intro-Section-With-Dropdown-Navigation/index.js
+++ Fontend Mentor Challenges/Intro-Section-With-Dropdown-Navigation/index.js
@@ -4,19 +4,22 @@ const showFeatures = document.getElementById("features-show");
 const showCompany = document.getElementById("company-show");
 const feauturesArrow = document.getElementById("arrow-features");
 const companyArrow = document.getElementById("arrow-company");
+const showMenuIcon = document.getElementById("show-menu-icon");
+const menuIcon = document.getElementById("menu-icon");
+const closeIcon = document.getElementById("close-icon");
+const navInfoLogin = document.querySelector(".nav-info-login");
 
 const arrowClickedStyle = "arrow-clicked";
 let featuresTtemsState = false;
 let companyItemsListState = false;
-
-console.log(featuresTtemsList.style.display);
+let isMenuOpen = false;
 
 showFeatures.addEventListener("click", function(){
     feauturesArrow.classList.toggle(arrowClickedStyle);
     if (featuresTtemsState) {
         featuresTtemsList.style.display = "none";
     } else {
-        featuresTtemsList.style.display = "block";
+        featuresTtemsList.style.display = "grid";
     }
     featuresTtemsState = !featuresTtemsState;
 });
@@ -26,7 +29,46 @@ showCompany.addEventListener("click", function(){
     if (companyItemsListState) {
         companyItemsList.style.display = "none";
     } else {
-        companyItemsList.style.display = "block";
+        companyItemsList.style.display = "grid";
     }
     companyItemsListState = !companyItemsListState;
+});
+
+showMenuIcon.addEventListener("click", ()=> {
+    if (isMenuOpen) {
+        closeIcon.style.display = "none";
+        menuIcon.style.display =  "block";
+        navInfoLogin.style.display = "none";
+        document.body.style.backgroundColor = "white";
+    } else {
+        closeIcon.style.display = "block";
+        menuIcon.style.display =  "none";
+        navInfoLogin.style.display = "flex";
+        document.body.style.backgroundColor = "#333333";
+    }
+    isMenuOpen = !isMenuOpen;
+})
+
+document.onreadystatechange = ()=> {
+    const mobileImg = document.getElementById("mobile-cover-img");
+    const desktopImg = document.getElementById("desktop-cover-img");
+    if(window.innerWidth > 850) {
+        mobileImg.style.display = "none";
+        desktopImg.style.display = "block";
+    } else {
+        mobileImg.style.display = "block";
+        desktopImg.style.display = "none";
+    }
+}
+
+window.addEventListener("resize", ()=> {
+    const mobileImg = document.getElementById("mobile-cover-img");
+    const desktopImg = document.getElementById("desktop-cover-img");
+    if(window.innerWidth > 850) {
+        mobileImg.style.display = "none";
+        desktopImg.style.display = "block";
+    } else {
+        mobileImg.style.display = "block";
+        desktopImg.style.display = "none";
+    }
 });
